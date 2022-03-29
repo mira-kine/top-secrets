b7da88760f54de253b94e5c7ca16fbd16c3a2f27
mira-kine <mirak0419@gmail.com>
2022-03-29 11:49:16 -0700

    wrote test for creating user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// Dummy user for testing
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: 'asdfg',
+};
+// create registerAndLogin that will check for whether use pw match
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+
+//   // create agent to store cookies between requests
+//   const agent = request.agent(app);
+//   // create user to sign in with
+// }
+// sign in
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -10,5 +26,17 @@ describe('top-secrets routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
